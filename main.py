42741d4308356635cbe06c4dc1bba4f6fb504e26
Amirabs <amirhosseinabbasifar@gmail.com>
2024-09-29 16:42:05 +0330

    Improve performance.

--- main.py
+++ main.py
@@ -13,8 +13,10 @@ cam = cv2.VideoCapture('rtsp://admin:admin1234@192.168.0.33:554/cam/realmonitor?
 last_center = None
 last_check_time = datetime.now()
 last_detected_time = datetime.now()
-threshold = 15  
+threshold = 55  
 missing_threshold = 10  
+min_side_length = 12
+area_limit = 180
 
 def is_approximate_hexagon(approx):
     if len(approx) != 6:
@@ -24,10 +26,15 @@ def is_approximate_hexagon(approx):
         return False
     
     angles = []
+    side_lengths = []
+    
     for i in range(6):
         p1 = approx[i][0]
         p2 = approx[(i + 1) % 6][0]
         p3 = approx[(i + 2) % 6][0]
+        
+        side_length = np.linalg.norm(p1 - p2)
+        side_lengths.append(side_length)
         
         vec1 = p1 - p2
         vec2 = p3 - p2
@@ -38,6 +45,10 @@ def is_approximate_hexagon(approx):
     
     for angle in angles:
         if angle < 55 or angle > 155:
+            return False
+
+    for length in side_lengths:
+        if length < min_side_length:
             return False
     
     return True
@@ -51,6 +62,10 @@ while True:
 
     blurred = cv2.GaussianBlur(gray, (3, 3), 6)
 
+    #median = cv2.medianBlur(blurred, 3)
+
+    #bilateral = cv2.bilateralFilter(median, 3, 50, 50)
+
     thresh = cv2.adaptiveThreshold(blurred, 150, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 5, 2)
 
     contours, hierarchy = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
@@ -59,12 +74,12 @@ while True:
 
     for contour in contours:
         # Detect
-        epsilon = 0.017 * cv2.arcLength(contour, True) # True = Closed Lines
-        approx = cv2.approxPolyDP(contour, epsilon, True) # R
+        epsilon = 0.0175 * cv2.arcLength(contour, True) ### ATTENTION: For 1080p: 0.015    For 720p: 0.017 ###
+        approx = cv2.approxPolyDP(contour, epsilon, True) # True = Closed Lines    
 
         if is_approximate_hexagon(approx):
             area = cv2.contourArea(contour)
-            if area > 300:  #by pixel
+            if area > area_limit:  # by pixel
 
                 # For Area of the contour
                 M = cv2.moments(contour) #m00 = pixels in contour -- m10 & m01 for center
