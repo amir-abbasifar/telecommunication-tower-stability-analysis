c897a9ad449676caf198a924f19a5f396f836d8d
Amirabs <amirhosseinabbasifar@gmail.com>
2024-09-29 17:39:47 +0330

    [DELETE] logfile - [DELETE] 'is_circle' and 'find_nearby_circles' and 'is_pentagon' methods - [ADD] .csv output - [ADD] 'count_unique_positions' method - [RESOLVE] problem of drawing regions.

--- main.py
+++ main.py
@@ -1,25 +1,25 @@
 import cv2
 import numpy as np
+import time
+import pandas as pd
 from datetime import datetime, timedelta
 from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtCore import QTimer, Qt
 from PyQt5.QtWidgets import QLabel, QVBoxLayout, QPushButton, QWidget, QApplication, QTextEdit
 from PyQt5.QtGui import QImage, QPixmap
 
-log = open("shape_movement_log.txt", mode="a")
-pic_num = 0
 #cam = cv2.VideoCapture('rtsp://admin:admin1234@192.168.0.33:554/cam/realmonitor?channel=1&subtype=0')
 cam = cv2.VideoCapture(0)
+cam_width = int(cam.get(cv2.CAP_PROP_FRAME_WIDTH))
+cam_height = int(cam.get(cv2.CAP_PROP_FRAME_HEIGHT))
 last_center = None
 last_check_time = datetime.now()
 last_detected_time = datetime.now()
-threshold = 50  
+threshold = 1  
 missing_threshold = 10  
 min_side_length = 5.5
 area_limit = 85
-distance_threshold = 50
-movement_check_interval = 5
-hexagon_positions = []
+distance_threshold = 120
 drawing = False
 ix, iy = -1, -1
 rx1, ry1, rx2, ry2 = 0, 0, 0, 0
@@ -50,35 +50,7 @@ def is_approximate_hexagon(approx):
             return False
     return True
 
-def is_circle(contour):
-    (x, y), radius = cv2.minEnclosingCircle(contour)
-    area = cv2.contourArea(contour)
-    circle_area = np.pi * (radius ** 2)
-    return abs(circle_area - area) / circle_area < 0.22 # Adjust tolerance as needed 
-
-def is_pentagon(contour):
-    epsilon = 0.02 * cv2.arcLength(contour, True)
-    approx = cv2.approxPolyDP(contour, epsilon, True)
-    
-    if len(approx) == 5:
-        angles = []
-        for i in range(5):
-            p1 = approx[i][0]
-            p2 = approx[(i + 1) % 5][0]
-            p3 = approx[(i + 2) % 5][0]
-            
-            v1 = p1 - p2
-            v2 = p3 - p2
-            
-            angle = np.degrees(np.arccos(np.dot(v1, v2) / (np.linalg.norm(v1) * np.linalg.norm(v2))))
-            angles.append(angle)
-        
-        avg_angle = np.mean(angles)
-        if all(abs(angle - avg_angle) < 10 for angle in angles) and 100 <= avg_angle <= 115:
-            return True
-    return False
-
-def is_star(contour, num_vertices=10, angle_threshold=10, side_length_threshold=0.2):
+def is_star(contour, num_vertices=10, angle_threshold=10, side_length_threshold=0.4):
     epsilon = 0.030 * cv2.arcLength(contour, True)
     approx = cv2.approxPolyDP(contour, epsilon, True)
     
@@ -120,7 +92,6 @@ def is_star(contour, num_vertices=10, angle_threshold=10, side_length_threshold=
 
 def detect_shapes(contours):
     hexagons = []
-    circles = []
     stars = []
     for contour in contours:
         epsilon = 0.022 * cv2.arcLength(contour, True)
@@ -131,32 +102,10 @@ def detect_shapes(contours):
                 hexagons.append(contour)
         elif is_star(contour):
             stars.append(contour)
-        elif is_circle(contour):
-            circles.append(contour)
-    return hexagons, circles, stars
+    return hexagons, stars
 
 def calculate_distance(p1, p2):
     return np.sqrt((p1[0] - p2[0]) ** 2 + (p1[1] - p2[1]) ** 2)
-
-def find_nearby_circles(hexagons, circles, distance_threshold):
-    nearby_circles = []
-    for circle in circles:
-        M = cv2.moments(circle)
-        if M["m00"] != 0:
-            cx = int(M["m10"] / M["m00"])
-            cy = int(M["m01"] / M["m00"])
-            circle_center = (cx, cy)
-            for hexagon in hexagons:
-                M_hex = cv2.moments(hexagon)
-                if M_hex["m00"] != 0:
-                    hx = int(M_hex["m10"] / M_hex["m00"])
-                    hy = int(M_hex["m01"] / M_hex["m00"])
-                    hexagon_center = (hx, hy)
-                    distance = calculate_distance(circle_center, hexagon_center)
-                    if distance < distance_threshold:
-                        nearby_circles.append(circle)
-                        break
-    return nearby_circles
 
 def check_movement(new_positions, old_positions, threshold):
     for new_pos in new_positions:
@@ -168,6 +117,18 @@ def check_movement(new_positions, old_positions, threshold):
         if moved:
             return True
     return False
+
+def count_unique_positions(centers):
+    unique_centers = []
+    for center in centers:
+        is_unique = True
+        for unique_center in unique_centers:
+            if np.linalg.norm(np.array(center) - np.array(unique_center)) < threshold:
+                is_unique = False
+                break
+        if is_unique:
+            unique_centers.append(center)
+    return len(unique_centers)
 
 class VideoWindow(QWidget):
     def __init__(self):
@@ -218,15 +179,22 @@ class VideoWindow(QWidget):
 
         self.last_check_time = datetime.now()
         self.last_detected_time = datetime.now()
-        self.hexagon_positions = []
         self.rect_ready = False
 
         self.region_last_check_times = {}
         self.region_hexagon_positions = {}
         self.region_last_detected_times = {}
         self.region_last_alarm_times = {}
+        self.region_last_count_time = {}
 
         self.is_started = False
+
+        self.log_df = pd.DataFrame(columns=["timestamp", "region", "hexagons", "stars"])
+
+    def closeEvent(self, event):
+        time_now = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+        self.log_df.to_csv(f"{time_now}.csv", index=False)
+        event.accept()
 
 
     def start_video(self):
@@ -272,7 +240,7 @@ class VideoWindow(QWidget):
             self.info_box.append("No region selected!")
             return
 
-        new_region = (min(self.rx1, self.rx2), min(self.ry1, self.ry2), max(self.rx1, self.rx2), max(self.ry1, self.ry2))
+        new_region = (max(0, min(self.rx1, self.rx2)), max(0, min(self.ry1, self.ry2)), min(cam_width, max(self.rx1, self.rx2)), min(cam_height, max(self.ry1, self.ry2)))
         
         for region in self.regions:
             if (abs(new_region[0] - region[0]) < 5 and 
@@ -304,92 +272,74 @@ class VideoWindow(QWidget):
         ret, frame = cam.read()
         if ret:
             for region in self.regions:
-                x1, x2 = min(region[0], region[2]), max(region[0], region[2])
-                y1, y2 = min(region[1], region[3]), max(region[1], region[3])
-
-                if x1 < x2 and y1 < y2:
-                    roi = frame[y1:y2, x1:x2]
-                else:
-                    roi = frame
+                x1, x2 = max(0, min(region[0], region[2])), min(cam_width, max(region[0], region[2]))
+                y1, y2 = max(0, min(region[1], region[3])), min(cam_height, max(region[1], region[3]))
+                roi = frame[y1:y2, x1:x2]
 
                 gray = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
                 blurred = cv2.GaussianBlur(gray, (3, 3), 5)
                 thresh = cv2.adaptiveThreshold(blurred, 150, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 5, 2)
                 contours, hierarchy = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-                detected_hexagons, detected_circles, detected_stars = detect_shapes(contours)
-                nearby_circles = find_nearby_circles(detected_hexagons, detected_circles, distance_threshold)
+                detected_hexagons, detected_stars = detect_shapes(contours)
 
-                current_positions = []
+                if region not in self.region_last_check_times:
+                    self.region_last_check_times[region] = datetime.now()
+                if region not in self.region_last_detected_times:
+                    self.region_last_detected_times[region] = datetime.now()
+                if region not in self.region_last_alarm_times:
+                    self.region_last_alarm_times[region] = datetime.now()
+                if region not in self.region_hexagon_positions:
+                    self.region_hexagon_positions[region] = []
+
+                hexagons_last_centers = []
+                stars_last_centers = []
+
                 for hexagon in detected_hexagons:
                     M = cv2.moments(hexagon)
                     if M["m00"] != 0:
                         cx = int(M["m10"] / M["m00"])
                         cy = int(M["m01"] / M["m00"])
-                        current_positions.append((cx, cy))
+                        hexagons_last_centers.append((cx, cy))
                         cv2.circle(roi, (cx, cy), 3, (0, 0, 255), -1)
-
-                if region not in self.region_last_check_times:
-                    self.region_last_check_times[region] = datetime.now()
-
-                if region not in self.region_hexagon_positions:
-                    self.region_hexagon_positions[region] = current_positions
-
-                if region not in self.region_last_detected_times:
-                    self.region_last_detected_times[region] = datetime.now()
-
-                if region not in self.region_last_alarm_times:
-                    self.region_last_alarm_times[region] = datetime.now()
-
-                if datetime.now() - self.region_last_check_times[region] > timedelta(seconds=movement_check_interval):
-                    if check_movement(current_positions, self.region_hexagon_positions[region], threshold):
-                        self.info_box.append(f"Shape Moved in region {region}!")
-
-                    self.region_hexagon_positions[region] = current_positions
-                    self.region_last_check_times[region] = datetime.now()
-
-                for circle in nearby_circles:
-                    M = cv2.moments(circle)
-                    if M["m00"] != 0:
-                        cx = int(M["m10"] / M["m00"])
-                        cy = int(M["m01"] / M["m00"])
-                        circle_center = (cx, cy)
-                        cv2.circle(roi, (cx, cy), 2, (0, 255, 0), -1)
 
                 for star in detected_stars:
                     M = cv2.moments(star)
                     if M["m00"] != 0:
                         cx = int(M["m10"] / M["m00"])
                         cy = int(M["m01"] / M["m00"])
-                        star_center = (cx, cy)
+                        stars_last_centers.append((cx, cy))
                         cv2.circle(roi, (cx, cy), 2, (255, 255, 0), -1)
 
-                if not detected_hexagons:
-                    if (datetime.now() - self.region_last_detected_times[region]).seconds > missing_threshold:
-                        if (datetime.now() - self.region_last_alarm_times[region]).seconds > missing_threshold:
-                            self.info_box.append(f"ALARM: Shape not detected in region {region}!")
-                            self.region_last_alarm_times[region] = datetime.now()
-                else:
-                    self.region_last_detected_times[region] = datetime.now()
+                hexagons_unique_centers = count_unique_positions(hexagons_last_centers)
+                stars_unique_centers = count_unique_positions(stars_last_centers)
+
+                current_time = time.time()
+                if region not in self.region_last_count_time:
+                    self.region_last_count_time[region] = current_time
+
+                if current_time - self.region_last_count_time[region] >= 5:
+                    time_now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+                    new_log = {"timestamp": time_now, "region": region, "hexagons": hexagons_unique_centers, "stars": stars_unique_centers}
+                    self.log_df = self.log_df._append(new_log, ignore_index=True)
+                    self.info_box.append(f"Region {region}: Hexagons: {hexagons_unique_centers}, Stars: {stars_unique_centers}")
+                    self.region_last_count_time[region] = current_time
 
                 cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
+                cv2.putText(frame, f"({x1}, {y1}, {x2}, {y2})", (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 1, cv2.LINE_AA)
 
-                text = f"({x1}, {y1}, {x2}, {y2})"
+                        
+            if self.drawing or (self.rx1 != 0 and self.ry1 != 0 and self.rx2 != 0 and self.ry2 != 0):
+                cv2.rectangle(frame, (self.rx1, self.ry1), (self.rx2, self.ry2), (0, 255, 0), 2)
+                text = f"({self.rx1}, {self.ry1}, {self.rx2}, {self.ry2})"
                 font = cv2.FONT_HERSHEY_SIMPLEX
-                cv2.putText(frame, text, (x1, y1 - 10), font, 0.5, (0, 255, 0), 1, cv2.LINE_AA)
-                    
-        if self.drawing or (self.rx1 != 0 and self.ry1 != 0 and self.rx2 != 0 and self.ry2 != 0):
-            cv2.rectangle(frame, (self.rx1, self.ry1), (self.rx2, self.ry2), (0, 255, 0), 2)
-
-            text = f"({self.rx1}, {self.ry1}, {self.rx2}, {self.ry2})"
-            font = cv2.FONT_HERSHEY_SIMPLEX
-            cv2.putText(frame, text, (self.rx1, self.ry1 - 10), font, 0.5, (0, 255, 0), 1, cv2.LINE_AA)
+                cv2.putText(frame, text, (self.rx1, self.ry1 - 10), font, 0.5, (0, 255, 0), 1, cv2.LINE_AA)
 
 
-        qformat = QImage.Format_RGB888
-        out_image = QImage(frame, frame.shape[1], frame.shape[0], frame.strides[0], qformat)
-        out_image = out_image.rgbSwapped()
-        self.video_label.setPixmap(QPixmap.fromImage(out_image))
+            qformat = QImage.Format_RGB888
+            out_image = QImage(frame, frame.shape[1], frame.shape[0], frame.strides[0], qformat)
+            out_image = out_image.rgbSwapped()
+            self.video_label.setPixmap(QPixmap.fromImage(out_image))
 
     def is_within_regions(self, x, y):
         for region in self.regions:
