a996bdc7d816104026f583f6af3777da2a76750f
Amirabs <amirhosseinabbasifar@gmail.com>
2024-09-29 18:20:01 +0330

    [ADD] state machine with states {'init' 'slow_scan' 'medium_scan' 'fast_scan' 'decision' 'yellow' 'red_calibration'} - [ADD] status circle - [CHANGE] draw region coordinates.

--- main.py
+++ main.py
@@ -8,14 +8,14 @@ from PyQt5.QtCore import QTimer, Qt
 from PyQt5.QtWidgets import QLabel, QVBoxLayout, QPushButton, QWidget, QApplication, QTextEdit, QSlider
 from PyQt5.QtGui import QImage, QPixmap
 
-cam = cv2.VideoCapture('rtsp://admin:admin1234@2.144.231.28:554/cam/realmonitor?channel=1&subtype=0')
-#cam = cv2.VideoCapture(0)
+#cam = cv2.VideoCapture('rtsp://admin:admin1234@2.144.231.28:554/cam/realmonitor?channel=1&subtype=0')
+cam = cv2.VideoCapture(0)
 cam_width = int(cam.get(cv2.CAP_PROP_FRAME_WIDTH))
 cam_height = int(cam.get(cv2.CAP_PROP_FRAME_HEIGHT))
 last_center = None
 last_check_time = datetime.now()
 last_detected_time = datetime.now()
-threshold = 1
+threshold = 10
 missing_threshold = 10  
 min_side_length = 2
 area_limit = 50
@@ -23,6 +23,7 @@ distance_threshold = 120
 drawing = False
 ix, iy = -1, -1
 rx1, ry1, rx2, ry2 = 0, 0, 0, 0
+slow_scan = 3
 
 def is_approximate_hexagon(approx):
     if len(approx) != 6:
@@ -107,11 +108,11 @@ def detect_shapes(contours, epsilon_val):
 def calculate_distance(p1, p2):
     return np.sqrt((p1[0] - p2[0]) ** 2 + (p1[1] - p2[1]) ** 2)
 
-def check_movement(new_positions, old_positions, threshold):
+def check_movement(new_positions, old_positions, given_thresh):
     for new_pos in new_positions:
         moved = True
         for old_pos in old_positions:
-            if calculate_distance(new_pos, old_pos) < threshold:
+            if calculate_distance(new_pos, old_pos) < given_thresh:
                 moved = False
                 break
         if moved:
@@ -128,7 +129,7 @@ def count_unique_positions(centers):
                 break
         if is_unique:
             unique_centers.append(center)
-    return len(unique_centers)
+    return unique_centers
 
 class VideoWindow(QWidget):
     def __init__(self):
@@ -218,6 +219,35 @@ class VideoWindow(QWidget):
 
         self.pic_num = 0
 
+        self.scan_time = 1
+
+        
+        ####################################### state
+        self.status_label = QLabel("Status:", self)
+        self.status_label.setGeometry(1400, 500, 50, 50)
+        self.status_label = QLabel(self)
+        self.status_label.setGeometry(1450, 500, 50, 50)
+        self.update_status("white")
+
+
+
+        self.state = "init"
+        self.flag_for_find_true_hexagons = 0
+        self.hexagons_last_unique_centers = []
+        self.hexagon_true_unique_centers = []
+        self.counter_flag = 0
+        self.flag_counter_green = 0
+        self.flag_counter_yellow = 0
+        self.flag_counter_red = 0
+
+        self.scan_time_slow = 3
+        self.scan_time_fast = 0.1
+        self.scan_time_medium = 0.3
+        self.number_of_fast_scan = 50
+        self.number_of_medium_scan = 50
+        self.flag_for_find_true_hexagons_limit = 3
+        ######################################## state            
+
     def update_epsilon(self):
         self.epsilon_value = float(float(self.epsilon_slider.value()) / 1000.0)
         self.epsilon_label.setText(f'Epsilon: {self.epsilon_value:.3f}')
@@ -243,6 +273,16 @@ class VideoWindow(QWidget):
             self.epsilon_slider.setEnabled(True)
         else:
             self.epsilon_slider.setEnabled(False)
+
+    def update_status(self, color):
+        if color == "green":
+            self.status_label.setStyleSheet("background-color: green; border-radius: 25px;")
+        elif color == "yellow":
+            self.status_label.setStyleSheet("background-color: yellow; border-radius: 25px;")
+        elif color == "red":
+            self.status_label.setStyleSheet("background-color: red; border-radius: 25px;")
+        elif color == "white":
+            self.status_label.setStyleSheet("background-color: white; border-radius: 25px;")
 
 
     def closeEvent(self, event):
@@ -299,14 +339,13 @@ class VideoWindow(QWidget):
                 self.update_frame()
 
     def add_region(self):
-        # if self.rx1 == self.rx2 or self.ry1 == self.ry2:
-        #     self.info_box.append("No region selected!")
-        #     return
+        if self.rx1 == self.rx2 or self.ry1 == self.ry2:
+            self.info_box.append("No region selected!")
+            return
 
-        #new_region = (max(0, min(self.rx1, self.rx2)), max(0, min(self.ry1, self.ry2)), min(cam_width, max(self.rx1, self.rx2)), min(cam_height, max(self.ry1, self.ry2)))
-        new_region = (1176,103,1260,163)
+        new_region = (max(0, min(self.rx1, self.rx2)), max(0, min(self.ry1, self.ry2)), min(cam_width, max(self.rx1, self.rx2)), min(cam_height, max(self.ry1, self.ry2)))
+        #new_region = (1176,103,1260,163)
 
-        
         for region in self.regions:
             if (abs(new_region[0] - region[0]) < 5 and 
                 abs(new_region[1] - region[1]) < 5 and 
@@ -328,6 +367,8 @@ class VideoWindow(QWidget):
             self.info_box.append("Last region deleted.")
             self.add_button.setEnabled(True)
             self.rect_ready = False
+            self.update_status("white")
+            self.state = "init"
             self.update_frame()
         
         self.rx1, self.ry1, self.rx2, self.ry2 = 0, 0, 0, 0
@@ -387,16 +428,114 @@ class VideoWindow(QWidget):
                 if not self.epsilon_checkbox.isChecked():
                     self.adjust_epsilon(len(detected_hexagons))
 
+########################################################################################################                    
+
+                if current_time - self.region_last_count_time[region] >= self.scan_time:
+
+                    if self.state == "init":
+                        if self.counter_flag == 15:
+                            self.rx1 = 0
+                            self.rx2 = cam_width
+                            self.ry1 = 0
+                            self.ry2 = cam_height
+                            self.add_region()
+                            self.counter_flag = 0
+                        else:
+                            self.counter_flag += 1
+
+                        if self.flag_for_find_true_hexagons < self.flag_for_find_true_hexagons_limit:
+                            if self.hexagons_last_unique_centers and not check_movement(hexagons_unique_centers, self.hexagons_last_unique_centers, threshold):
+                                self.flag_for_find_true_hexagons += 1
+                        else:
+                            self.state = "slow_scan"
+                            self.hexagon_true_unique_centers = hexagons_unique_centers
+                            self.update_status("green")
+
+                    elif self.state == "slow_scan":
+                        self.scan_time = self.scan_time_slow
+                        if not check_movement(hexagons_unique_centers, self.hexagon_true_unique_centers, threshold):
+                            pass
+                        else:
+                            self.state = "fast_scan"
+                            self.scan_time = self.scan_time_fast
+                            self.counter_flag = 0
+
+                    elif self.state == "fast_scan":
+                        if not self.counter_flag == self.number_of_fast_scan:
+                            if check_movement(hexagons_unique_centers, self.hexagon_true_unique_centers, threshold):
+                                if check_movement(hexagons_unique_centers, self.hexagons_last_unique_centers, threshold):
+                                    self.flag_counter_yellow += 1
+                                else:
+                                    self.flag_counter_red += 1
+                            else:
+                                self.flag_counter_green += 1
+                            self.counter_flag += 1
+
+                        else:
+                            self.state = "decision"
+                            self.counter_flag = 0
+                            self.info_box.append("Decision Decision Decision Decision Decision Decision Decision")
 
 
-                if current_time - self.region_last_count_time[region] >= 3:
-                    time_now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-                    new_log = {"timestamp": time_now, "region": region, "hexagons": hexagons_unique_centers, "stars": stars_unique_centers}
-                    self.log_df = self.log_df._append(new_log, ignore_index=True)
-                    self.info_box.append(f"Region {region}: Hexagons: {hexagons_unique_centers}, Stars: {stars_unique_centers}")
-                    cv2.imwrite(f"Picture_moved{self.pic_num}.jpg", frame)
-                    self.pic_num = self.pic_num + 1
+                    elif self.state == "decision":
+                        if max(self.flag_counter_green, self.flag_counter_red, self.flag_counter_yellow) == self.flag_counter_green:
+                            self.flag_counter_yellow = 0
+                            self.flag_counter_red = 0
+                            self.flag_counter_green = 0
+                            self.state = "slow_scan"
+                            self.update_status("green")
+                        elif max(self.flag_counter_green, self.flag_counter_red, self.flag_counter_yellow) == self.flag_counter_red:
+                            self.flag_counter_yellow = 0
+                            self.flag_counter_red = 0
+                            self.flag_counter_green = 0
+                            self.update_status("red")
+                            self.state = "red_calibration"
+                        elif max(self.flag_counter_green, self.flag_counter_red, self.flag_counter_yellow) == self.flag_counter_yellow:
+                            self.flag_counter_yellow = 0
+                            self.flag_counter_red = 0
+                            self.flag_counter_green = 0
+                            self.state = "yellow"
+                            self.update_status("yellow")
+                            self.info_box.append("Warning: Unstable weather conditions.")
+
+                    elif self.state == "yellow":
+                        self.state = "medium_scan"
+                        self.scan_time = self.scan_time_medium
+                    
+                    elif self.state == "medium_scan":
+                        if not self.counter_flag == self.number_of_medium_scan:
+                            if check_movement(hexagons_unique_centers, self.hexagon_true_unique_centers, threshold):
+                                if check_movement(hexagons_unique_centers, self.hexagons_last_unique_centers, threshold):
+                                    self.flag_counter_yellow += 1
+                                else:
+                                    self.flag_counter_red += 1
+                            else:
+                                self.flag_counter_green += 1
+                            self.counter_flag += 1
+
+                        else:
+                            self.state = "decision"
+                            self.counter_flag = 0
+                    
+                    elif self.state == "red_calibration":
+                        self.state = "slow_scan"
+                        self.hexagon_true_unique_centers = hexagons_unique_centers
+
                     self.region_last_count_time[region] = current_time
+                    self.hexagons_last_unique_centers = hexagons_unique_centers
+
+
+##################################################################################################
+
+
+                # if current_time - self.region_last_count_time[region] >= self.scan_time:
+                #     time_now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+                #     new_log = {"timestamp": time_now, "region": region, "hexagons": len(hexagons_unique_centers), "stars": len(stars_unique_centers)}
+                #     self.log_df = self.log_df._append(new_log, ignore_index=True)
+                #     self.info_box.append(f"Region {region}: Hexagons: {len(hexagons_unique_centers)}, Stars: {len(stars_unique_centers)}")
+                #     cv2.imwrite(f"Picture_moved{self.pic_num}.jpg", frame)
+                #     self.pic_num = self.pic_num + 1
+                #     self.region_last_count_time[region] = current_time
 
                 cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
                 cv2.putText(frame, f"({x1}, {y1}, {x2}, {y2})", (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 1, cv2.LINE_AA)
